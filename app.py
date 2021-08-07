8be7b9c0fb70a7f59b62a0d5ea79c4f6b593df22
MaheshC <maheshmechenggi@gmail.com>
2021-08-07 15:07:50 +0530

    docker running

--- app.py
+++ app.py
@@ -1,38 +1,29 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Jun 13 02:20:31 2020
-
-@author: Krish Naik
-"""
-
-# -*- coding: utf-8 -*-
-"""
 Created on Fri May 15 12:50:04 2020
 
 @author: krish.naik
 """
 
-
+from flask import Flask, request
 import numpy as np
 import pickle
 import pandas as pd
-#from flasgger import Swagger
-import streamlit as st 
+import flasgger
+from flasgger import Swagger
 
-from PIL import Image
-
-#app=Flask(__name__)
-#Swagger(app)
+app=Flask(__name__)
+Swagger(app)
 
 pickle_in = open("classifier.pkl","rb")
 classifier=pickle.load(pickle_in)
 
-#@app.route('/')
+@app.route('/')
 def welcome():
     return "Welcome All"
 
-#@app.route('/predict',methods=["Get"])
-def predict_note_authentication(variance,skewness,curtosis,entropy):
+@app.route('/predict',methods=["Get"])
+def predict_note_authentication():
     
     """Let's Authenticate the Banks Note 
     This is using docstrings for specifications.
@@ -59,35 +50,38 @@ def predict_note_authentication(variance,skewness,curtosis,entropy):
             description: The output values
         
     """
-   
+    variance=request.args.get("variance")
+    skewness=request.args.get("skewness")
+    curtosis=request.args.get("curtosis")
+    entropy=request.args.get("entropy")
     prediction=classifier.predict([[variance,skewness,curtosis,entropy]])
     print(prediction)
-    return prediction
+    return "Hello The answer is"+str(prediction)
 
-
-
-def main():
-    st.title("Bank Authenticator")
-    html_temp = """
-    <div style="background-color:tomato;padding:10px">
-    <h2 style="color:white;text-align:center;">Streamlit Bank Authenticator ML App </h2>
-    </div>
+@app.route('/predict_file',methods=["POST"])
+def predict_note_file():
+    """Let's Authenticate the Banks Note 
+    This is using docstrings for specifications.
+    ---
+    parameters:
+      - name: file
+        in: formData
+        type: file
+        required: true
+      
+    responses:
+        200:
+            description: The output values
+        
     """
-    st.markdown(html_temp,unsafe_allow_html=True)
-    variance = st.text_input("Variance","Type Here")
-    skewness = st.text_input("skewness","Type Here")
-    curtosis = st.text_input("curtosis","Type Here")
-    entropy = st.text_input("entropy","Type Here")
-    result=""
-    if st.button("Predict"):
-        result=predict_note_authentication(variance,skewness,curtosis,entropy)
-    st.success('The output is {}'.format(result))
-    if st.button("About"):
-        st.text("Lets LEarn")
-        st.text("Built with Streamlit")
+    df_test=pd.read_csv(request.files.get("file"))
+    print(df_test.head())
+    prediction=classifier.predict(df_test)
+    
+    return str(list(prediction))
 
 if __name__=='__main__':
-    main()
-    
+    app.run()
+	#app.run(host='0.0.0.0',port=8000)
     
     
